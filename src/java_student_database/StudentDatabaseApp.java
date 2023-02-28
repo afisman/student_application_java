9754e00fc3015fdd5e168d60bb810be9e6a886eb
afisman <afisman@gmail.com>
2023-02-28 15:39:59 +0100

    added functionaity to create array of students with prompted number

--- src/java_student_database/StudentDatabaseApp.java
+++ src/java_student_database/StudentDatabaseApp.java
@@ -1,15 +1,32 @@
 package java_student_database;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student stu1 = new Student();
-		stu1.enroll();
-		stu1.payTuition();
-		System.out.println(stu1.toString());
-		// Ask how many users we want to add
+		
+		// Ask how many students we want to add
+		
+		System.out.println(" Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
 		
 		//Create n number of students
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].toString());
+		}
+		
+
 
 	}
 
